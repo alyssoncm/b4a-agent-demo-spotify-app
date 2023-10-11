6ae9479a1b8144d166d2c3f62ab376b18b1d8dd4
Back4App Demo <demo@back4app.com>
2023-10-11 14:17:41 +0000

    Update application with mocked data and Materialize CSS

---
+++ mockData.js
@@ -0,0 +1,4 @@
+const artists = [
+    { name: 'Artist 1', songs: 100, listeners: 500000, albums: ['Album 1', 'Album 2', 'Album 3'], topSongs: ['Song 1', 'Song 2', 'Song 3', 'Song 4', 'Song 5'] },
+    // ... add more artists here
+];
